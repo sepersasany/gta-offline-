9d8fb8c4edcfa38069b9dc589ade0bd1e9e3983a
sepersasany <sydsasany15@gmail.com>
2025-09-20 11:46:45 +0330

    Add Server folder placeholder

--- MainServer.cs
+++ MainServer.cs
@@ -2,3 +2,23 @@ Libs/Server/.gitkeep
 Libs/Client/.gitkeep
 Resources/Server/Scripts/MainServer.cs
 Resources/Server/Assets/.gitkeep
+using RageCoop.Server.Scripting;
+using RageCoop.Core;
+
+namespace GTA offline 
+{
+    public class MainServer : ServerScript
+    {
+        public override void OnStart()
+        {
+            API.Log("Server script started!");
+            // Initialize your server-side logic here
+        }
+
+        public override void OnStop()
+        {
+            API.Log("Server script stopped.");
+            // Clean up resources here
+        }
+    }
+}
